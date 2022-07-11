b05bde3adc72acbd9035a5ad9f022128bb728662
Marcus Herrera <maherreraiv1987@gmail.com>
2022-07-11 11:17:41 -0600

    added links to icons in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,35 @@ const FooterComponent = () => {
 	return (
 		<div className="relative">
 			<footer className="fixed bottom-0 left-0 right-0 flex flex-wrap items-center justify-center px-2 py-3 bg-neutral-800 opacity-90">
-				<img className="h-10 px-2" alt="GitHub logo" src={GitHub} />
-				<img className="h-10 px-2" alt="Twitter logo" src={Twitter} />
-				<img className="h-10 px-2" alt="LinkedIn logo" src={LinkedIn} />
+				<a
+					href="https://github.com/mahiv87"
+					target="_blank"
+					rel="noreferrer noopener"
+				>
+					<img className="h-10 px-2" alt="GitHub logo" src={GitHub} />
+				</a>
+				<a
+					href="https://twitter.com/mahiv87"
+					target="_blank"
+					rel="noreferrer noopener"
+				>
+					<img
+						className="h-10 px-2"
+						alt="Twitter logo"
+						src={Twitter}
+					/>
+				</a>
+				<a
+					href="https://www.linkedin.com/in/marcus-herrera-74453b227/"
+					target="_blank"
+					rel="noreferrer noopener"
+				>
+					<img
+						className="h-10 px-2"
+						alt="LinkedIn logo"
+						src={LinkedIn}
+					/>
+				</a>
 			</footer>
 		</div>
 	);
