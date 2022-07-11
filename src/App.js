9547d29e87bb40ca9b67ed8e61eda8973cb646fe
Marcus Herrera <maherreraiv1987@gmail.com>
2022-07-11 10:38:06 -0600

    background and nav added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Particles from './components/Particles';
+import NavBar from './components/NavBar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Particles id="tsparticles" />
+			<div className="header">
+				<NavBar />
+			</div>
+			{/* <div className="container"></div> */}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Particles.js
@@ -0,0 +1,51 @@
+import { useCallback, useMemo } from 'react';
+import Particles from 'react-tsparticles';
+import { loadSlim } from 'tsparticles-slim';
+
+const ParticlesComponent = (props) => {
+	const options = useMemo(() => {
+		return {
+			fullScreen: {
+				enable: true,
+				zIndex: -1
+			},
+			particles: {
+				links: {
+					enable: true,
+					distance: 150
+				},
+				move: {
+					enable: true
+				},
+				// collisions: {
+				// 	enable: true,
+				// 	mode: 'absorb'
+				// },
+				number: {
+					density: {
+						enable: true,
+						area: 1000
+					}
+				},
+				opacity: {
+					animation: {
+						enable: true
+					},
+					value: { min: 0.3, max: 1 },
+					speed: { min: 0.1, max: 1 }
+				},
+				size: {
+					value: { min: 0.2, max: 5 }
+				}
+			}
+		};
+	}, []);
+
+	const particlesInit = useCallback((engine) => {
+		loadSlim(engine);
+	}, []);
+
+	return <Particles id={props.id} init={particlesInit} options={options} />;
+};
+
+export default ParticlesComponent;
