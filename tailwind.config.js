3f0680ce8105c1d86980b611deee5c2281a8af09
Marcus Herrera <maherreraiv1987@gmail.com>
2022-07-12 19:48:43 -0600

    change header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {}
+		extend: {
+			fontSize: {
+				tiny: '.5rem'
+			}
+		}
 	},
 	plugins: [require('@tailwindcss/forms')]
 };
