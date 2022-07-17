115d2bb7652119f1735170803bb7cee3f3a133b0
Marcus mbp <maherreraiv1987@gmail.com>
2022-07-16 21:17:06 -0600

    debugged images not rendering

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,25 +10,25 @@ const Portfolio = () => {
 	const projects = [
 		{
 			name: 'Tech_Blog',
-			image: { TechBlog },
+			image: TechBlog,
 			repoLink: 'https://github.com/mahiv87/Tech-Blog',
 			deployedApp: 'https://calm-gorge-51353.herokuapp.com/'
 		},
 		{
 			name: 'Park_Pass',
-			image: { ParkPass },
+			image: ParkPass,
 			repoLink: 'https://github.com/gokublue007/Park-Pass',
 			deployedApp: 'https://gokublue007.github.io/Park-Pass/'
 		},
 		{
 			name: 'CSS_Cheatsheet',
-			image: { CssSnippet },
+			image: CssSnippet,
 			repoLink: 'https://github.com/mahiv87/CSS-Snippet-Cheatsheet',
 			deployedApp: 'https://mahiv87.github.io/CSS-Snippet-Cheatsheet/'
 		},
 		{
 			name: 'Password_Generator',
-			image: { PasswordGen },
+			image: PasswordGen,
 			repoLink:
 				'https://github.com/mahiv87/Super-Awesome-Password-Generator',
 			deployedApp:
@@ -36,13 +36,13 @@ const Portfolio = () => {
 		},
 		{
 			name: 'J.A.T.E',
-			image: { ProgressiveWebApp },
+			image: ProgressiveWebApp,
 			repoLink: 'https://github.com/mahiv87/Progressive-Text-Editor',
 			deployedApp: 'https://floating-mesa-79476.herokuapp.com/'
 		},
 		{
 			name: 'Team_Profile_Generator',
-			image: { TeamProfileGen },
+			image: TeamProfileGen,
 			repoLink: 'https://github.com/mahiv87/Team-Profile-Generator',
 			deployedApp: ''
 		}
